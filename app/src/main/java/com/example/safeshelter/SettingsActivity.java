534f9722d948f16b86abc18245fd8a4f4cc85a39
duartegoncalves123 <duarte59escola@gmail.com>
2019-11-26 22:15:29 +0000

    Ecrã Settings a funcionar Data: 25/11/2019

--- app/src/main/java/com/example/safeshelter/SettingsActivity.java
+++ app/src/main/java/com/example/safeshelter/SettingsActivity.java
@@ -1,32 +1,17 @@
 package com.example.safeshelter;
 
-import androidx.annotation.Nullable;
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.view.ViewGroup;
+import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
-        setTitle("SafeShelter");
-
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainSettingsFragment()).commit();
-    }
-
-    public static class MainSettingsFragment extends PreferenceFragment{
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            addPreferencesFromResource(R.xml.preferences);
-        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
