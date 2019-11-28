ed793cd70e23e8aa2b7689fa985fd87c84f682a5
duartegoncalves123 <duarte59escola@gmail.com>
2019-11-28 12:12:40 +0000

    Merge branches 'Development' and 'Settings' of https://github.com/LeGoonie/SafeShelterCM into Settings
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/safeshelter/SettingsActivity.java
+++ app/src/main/java/com/example/safeshelter/SettingsActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class SettingsActivity extends AppCompatActivity {
     public Preference profile_button;
+    public static final String SHARED_PREFS = "sharedPrefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,10 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
     }
 }
