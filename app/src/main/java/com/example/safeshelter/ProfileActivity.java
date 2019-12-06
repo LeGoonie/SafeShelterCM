69a08ef53833eb45adde67abbae6847124f01656
andrereis99 <andrereis99@users.noreply.github.com>
2019-12-06 20:25:22 +0000

    database update

--- app/src/main/java/com/example/safeshelter/ProfileActivity.java
+++ app/src/main/java/com/example/safeshelter/ProfileActivity.java
@@ -1,25 +1,35 @@
 package com.example.safeshelter;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
-    TextView mProfileTv;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    //views from xml
+    TextView nameTv, childrenNameTv, emailTv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTitle("Profile");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
@@ -40,25 +50,39 @@ public class ProfileActivity extends AppCompatActivity {
 
         //init
         firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
 
-        mProfileTv = findViewById(R.id.profileTv);
-    }
+        //init values
+        nameTv = findViewById(R.id.profileTv);
+        childrenNameTv = findViewById(R.id.TextView_childrenName);
+        emailTv = findViewById(R.id.TextView_email);
 
-    private void checkUserStatus(){
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if(user != null){
-            //user is signed in stay here
-            //set email of logged in user
-            mProfileTv.setText(user.getEmail());
-        } else{
-            startActivity(new Intent(ProfileActivity.this, HomeActivity.class));
-            finish();
-        }
-    }
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //check until required data get
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    if(ds.child("email").getValue().toString().equals(user.getEmail())){
+                        //get data
+                        String name = "" + ds.child("parentName").getValue().toString();
+                        String childName = "Children Name: " + ds.child("childrenName").getValue().toString();
+                        String email = "Email: " + ds.child("email").getValue().toString();
 
-    @Override
-    protected void onStart(){
-        checkUserStatus();
-        super.onStart();
+                        //set data
+                        nameTv.setText(name);
+                        childrenNameTv.setText(childName);
+                        emailTv.setText(email);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
